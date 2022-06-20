f942847bcef56b2d05b1cad9de30d00f5e8344f9
フランソワ <Francois_vd@icloud.com>
2022-06-21 00:14:55 +0800

    start with actual site

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,3 +1,15 @@
+import React from 'react'
+import { Button } from 'antd'
+
 export const Banner = () => {
-  return <div></div>
+  return (
+    <img
+      src={
+        'https://images.unsplash.com/photo-1653047256226-ab0d16c758d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'
+      }
+      // style={{ height: 600, as }}
+      height={600}
+      alt={'test'}
+    />
+  )
 }
